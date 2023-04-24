1f4916c7eb33306ffeae7e23447ec58d2eb59061
Dubskey <DUBBYW.PALADINO@GMAIL.COM>
2023-04-23 23:03:39 -0700

    Update Pause.cs

--- Arena++/Assets/Scripts/Pause.cs
+++ Arena++/Assets/Scripts/Pause.cs
@@ -14,7 +14,7 @@ public class Pause : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Q))
         {
             if (gameIsPaused)
             {
